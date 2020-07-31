00951822271a1387a0bc8dc102a42d37a22b84a2
Javier Lozano <ing.javierlozano@hotmail.com>
2020-07-30 21:33:20 -0500

    Se importa mongoose se conecta a la base de datos

--- API/index.js
+++ API/index.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 //Crear el servidor
 const app = express();
+
+//Conectar a mongodb
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb+srv://Javi:123@clusterdevjavi.2xtwz.mongodb.net/veterinaria', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+});
 
 //Puerto e iniciar el servidor
 app.listen(4000, () => {
