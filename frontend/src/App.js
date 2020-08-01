036759e8fb3b0b7128f43059818feabf88bffb6d
Javier Lozano <ing.javierlozano@hotmail.com>
2020-08-01 08:39:12 -0500

    Se consume la apli ca consulta

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+import clienteAxios from './config/axios';
+
 //Componentes
-import Pacientes from "./components/Pacientes";
-import NuevaCita from "./components/NuevaCita";
+import Pacientes from './components/Pacientes';
+import NuevaCita from './components/NuevaCita';
 import Cita from './components/Cita';
 
 function App() {
+  const [citas, setCitas] = useState([]);
+
+  useEffect(() => {
+    const consultaAPI = async () => {
+      const pacientes = await clienteAxios.get('/pacientes');
+      setCitas(pacientes.data);
+    };
+    consultaAPI();
+    return () => {};
+  }, []);
+
   return (
     <Router>
       <Switch>
